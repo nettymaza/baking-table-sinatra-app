4d37837444a8ba98b88ac8fbaa4471bb9b02a091
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-23 16:03:23 -0600

    Added primary routes to recipe controller. Created recipes.rb.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,16 @@
 class RecipesController < ApplicationController
+
+get "/recipes" do
+  @recipes = Recipe.all
+  erb :"/recipes/recipes"
+end
+
+get "/recipes/new" do
+
+end
+
+post "/recipes" do
+
+end
+
 end
