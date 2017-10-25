36096824ec8f78182fb54bc2d4e8926f908dafdf
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-25 15:36:31 -0600

    Added edit form and edit controller routes to recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,8 @@ require 'pry'
 require 'awesome_print'
 
 class RecipesController < ApplicationController
+
+
   get "/recipes" do
     @recipes = Recipe.all
     erb :"/recipes/recipes"
@@ -17,18 +19,42 @@ class RecipesController < ApplicationController
      erb :"/recipes/show_recipe"
    end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if @recipe.baker == current_baker
+      erb :"/recipes/edit_recipe"
+    else
+      redirect "/recipes/#{@recipe.id}"
+    end
+  end
+
   post "/recipes" do
+    # Need to validate creating a recipe!
     if logged_in?
       @recipe = Recipe.new
-      @recipe.baker = current_baker
       @recipe.name = params[:name]
       @recipe.ingredients = params[:ingredients]
       @recipe.instructions = params[:instructions]
+      @recipe.baker = current_baker
       @recipe.category_ids = params[:categories]
       @recipe.save
 
       redirect "/recipes/#{@recipe.id}"
     end
-
   end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if logged_in?
+      @recipe.name = params[:name]
+      @recipe.ingredients = params[:ingredients]
+      @recipe.instructions = params[:instructions]
+      @recipe.baker = current_baker
+      @recipe.category_ids = params[:categories]
+      @recipe.save
+
+      redirect "/recipes/#{@recipe.id}"
+    end
+  end
+
 end
