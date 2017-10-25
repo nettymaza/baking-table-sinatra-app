42c044cfaf7bc621e08c3be9857df2be8f8c0bfc
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-25 15:35:52 -0600

    Worked on categories views and controller.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+
+  get '/categories' do
+    @categories = Category.all
+    erb :'/categories/index'
+  end
+
+  get '/categories/:id' do
+    @category = Category.find_by_id(params[:id])
+    erb :'categories/show'
+  end
+
 end
