3f3dea19113253247fa1fb5323576e8aeaeb7af8
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-25 20:10:33 -0600

    Added recipe validation for creating a recipe.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,13 @@ require 'awesome_print'
 
 class RecipesController < ApplicationController
 
-
+  # Recipe Index
   get "/recipes" do
     @recipes = Recipe.all
     erb :"/recipes/recipes"
   end
 
+  #Create New Recipe
   get "/recipes/new" do
     @categories = Category.all
     erb :"/recipes/create_recipe"
@@ -19,26 +20,30 @@ class RecipesController < ApplicationController
      erb :"/recipes/show_recipe"
    end
 
+  post "/recipes" do
+    if logged_in?
+      if params[:name] == "" || params[:ingredients] == "" || params[:instructions] == ""
+        redirect '/recipes/new'
+      else
+        @recipe = Recipe.new
+        @recipe.name = params[:name]
+        @recipe.ingredients = params[:ingredients]
+        @recipe.instructions = params[:instructions]
+        @recipe.baker = current_baker
+        @recipe.category_id = params[:category_id]
+        @recipe.save
+
+        redirect "/recipes/#{@recipe.id}"
+      end
+    end
+  end
+
+  # Edit Recipe
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
     if @recipe.baker == current_baker
       erb :"/recipes/edit_recipe"
     else
-      redirect "/recipes/#{@recipe.id}"
-    end
-  end
-
-  post "/recipes" do
-    # Need to validate creating a recipe!
-    if logged_in?
-      @recipe = Recipe.new
-      @recipe.name = params[:name]
-      @recipe.ingredients = params[:ingredients]
-      @recipe.instructions = params[:instructions]
-      @recipe.baker = current_baker
-      @recipe.category_ids = params[:categories]
-      @recipe.save
-
       redirect "/recipes/#{@recipe.id}"
     end
   end
@@ -50,10 +55,21 @@ class RecipesController < ApplicationController
       @recipe.ingredients = params[:ingredients]
       @recipe.instructions = params[:instructions]
       @recipe.baker = current_baker
-      @recipe.category_ids = params[:categories]
+      @recipe.category_id = params[:category_id]
       @recipe.save
 
       redirect "/recipes/#{@recipe.id}"
+    end
+  end
+
+  # Delete Recipe
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if logged_in? && @recipe.baker_id == current_baker.id
+      @recipe.delete
+      redirect '/bakers/show_dashboard'
+    else
+      redirect '/login'
     end
   end
 
