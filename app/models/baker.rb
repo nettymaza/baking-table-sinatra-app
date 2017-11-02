72ac314081e012a5775192a0686a0f660459a529
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-11-02 17:44:38 -0600

    added validation methods to Baker class.

--- app/models/baker.rb
+++ app/models/baker.rb
@@ -1,5 +1,8 @@
 class Baker < ActiveRecord::Base
   has_secure_password
   has_many :recipes
-  has_many :categories, :through => :recipes
+  has_many :categories, through: :recipes
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
 end
